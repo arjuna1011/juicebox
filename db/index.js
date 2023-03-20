ebe8f5ebe17403d17c56b8c17adbec300ed99693
jgboatrighte <jgboatright@LAPTOP-24EEDVDM>
2023-03-19 22:48:07 -0400

    Final  commit hopefully

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const { Client } = require('pg');
-
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+require('dotnev').config()
+const client = new Client(process.env.DATABASE_URL);
+client.password="ouD22h9r8UMJWJ3Td7MokVDzjiC53kmq"
 
 async function getAllUsers() {
     const { rows } = await client.query(
